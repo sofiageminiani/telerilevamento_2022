7f1214de953d6b85e260072a23d74f5afb9042de
sofiageminiani <100956833+sofiageminiani@users.noreply.github.com>
2022-04-11 18:22:52 +0200

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -1,17 +1,20 @@
-# richiamo la libreria che mi servirà
+# richiamare le librerie che serviranno
 library(raster) 
-# install.packages("rgdal") perchè come Mac user mi veniva richiesto di scaricare questo pacchetto per visualizzare le immagini in estensione.jpg
-# la libreria rgdal non è da richiamare perchè fa parte di raster
+# install.packages("rgdal"), come Mac user mi veniva richiesta l'installazione di questo pacchetto
 # install.packages("RStoolbox)
 # install.packages(“rasterdiv”)
+# libray(rgdal) non è da richiamare perchè fa parte del pacchetto raster
 library(RStoolbox)
 library(rasterdiv)
 
-setwd("/Users/sofiageminiani/desktop/lab") #settiamo la cartella di lavoro, Mac users
+#settare la cartella di lavoro - working directory
+setwd("/Users/sofiageminiani/desktop/lab") # Mac users
+# setwd("~/lab/") # Linux
+# setwd("C:/lab/") # Windows
 
 # importare/caricare l'immagine satellitare da analizzare nominandola l1992
 l1992 <- brick("defor1_.jpg")
-l1992 # cliccando invio otterrò una serie di informazioni riportate nei commenti sottostanti
+l1992 # cliccando invio si ottengo le informazioni sottostanti 
 ## class      : RasterBrick 
 ## dimensions : 478, 714, 341292, 3  (nrow, ncol, ncell, nlayers) 
 ## resolution : 1, 1  (x, y) 
@@ -21,21 +24,24 @@ l1992 # cliccando invio otterrò una serie di informazioni riportate nei comment
 ## names      : defor1_.1, defor1_.2, defor1_.3 
 ## min values :         0,         0,         0 
 ## max values :       255,       255,       255 
-# dalle informazioni possiamo trarre che abbiamo a disposizione tre bande, che la risoluzione è di tipo immagine perchè non è georeferenziata e le bande si chiamano defor1_.1, defor1_.2 e defor1_.3 
-# riguardo i valori massimi e minimi vediamo che i valori vanno da 0 a 255, usiamo Shannon
+# dalle informazioni possiamo trarre che abbiamo a disposizione tre bande
+# che la risoluzione è di tipo immagine, perchè non è georeferenziata
+# le bande si chiamano defor1_.1, defor1_.2 e defor1_.3 
+# i valori massimi e minimi vanno da 0 a 255
+
 
 # come facciamo a capire  quali sono le bande coinvolte? facciamo un plotRGB()
 # scoprire quale banda porta la vegetazione a plottarla di colore rosso.
 
-plotRGB(l1992, r=1, g=2, b=3, stretch=“lin”) # questa funzione fa parte del pacchetto raster; le componeneti sono ipotetiche perchè non sappiamo che bande abbiamo dentro
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin") # questa funzione fa parte del pacchetto raster; le componeneti sono ipotetiche perchè non sappiamo che bande abbiamo dentro
 # essendo il plot rosso vuol dire che l'infrarosso vicino è nella componente 1, le successive in sequenza
 # layer 1 = NIR
 # layer 2 = red
 # layer 3 = green
 
-# importare/caricare la seconda immagine satellitare da analizzare nominandola l2006
-l2006 <- brick(“defor2_.jpg”)
-l2006 # se clicco invio otterrò le informazioni riportate di seguito
+# importare la seconda immagine satellitare da analizzare nominandola l2006
+l2006 <- brick("defor2_.jpg")
+l2006 # cliccando invio si ottengo le informazioni riportate di seguito
 ## class      : RasterBrick 
 ## dimensions : 478, 717, 342726, 3  (nrow, ncol, ncell, nlayers)
 ## resolution : 1, 1  (x, y)
@@ -47,21 +53,22 @@ l2006 # se clicco invio otterrò le informazioni riportate di seguito
 ## max values :       255,       255,       255
 
 # scoprire quale banda porta la vegetazione a plottarla di colore rosso.
-plotRGB(l2006, r=1, g=2, b=3, stretch=“lin”)
+plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 
 # plottare in un multiframe le immagini del 1992 e del 2006 una sopra l'altra
 par(mfrow=c(2,1))
-plotRGB(l1992, r=1, g=2, b=3, stretch=“lin”)
-plotRGB(l2006, r=1, g=2, b=3, stretch=“lin”)
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 # si vede bene la foresta del 1992, mentre nella immagine del 2006 si vedono bene i campi
 # di solito l'acqua è nera perchè assorbe tutta la luce, mentre in queste immagini è di colore chiaro (simile a quella del suolo nudo) per via della presenza del sedimento
+
 
 # calcoliamo un indice spettrale: il DVI, Difference Vegetation Index
 # calcoliamo il DVI, la salute della vegetazione, per l'anno 1992
 dvi1992 = l1992[[1]] - l1992[[2]] #in questo caso potevamo usare il simbolo di assegnazione "<-"
 # oppure
 dvi1992 = l1992$defor1_.1 - l1992$defor1_.2
-dvi1992 # se clicco invio otterrò le informazioni riportate di seguito
+dvi1992 # cliccando invio si ottengo le informazioni riportate di seguito
 ## class      : RasterLayer 
 ## dimensions : 478, 714, 341292  (nrow, ncol, ncell)
 ## resolution : 1, 1  (x, y)
@@ -74,14 +81,14 @@ dvi1992 # se clicco invio otterrò le informazioni riportate di seguito
 #plottiamo il dvi1992 utilizzando una palette di colori personalizzata per la legenda
 cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
 plot(dvi1992, col=cl)
-# visualizzazione imagine, tutto quello che è rosso scuro è in salute, mentre ciò che è giallo non è in salute
-# il fiume è giallo per la presenza di sedimenti al suo interno
+# visualizzazione imagine: tutto quello che è rosso scuro è in salute, mentre ciò che è giallo non è in salute
+# il fiume è giallo per la presenza di sedimenti disciolti
 
 # calcoliamo il DVI, la salute della vegetazione, per l'anno 2006
 dvi2006 = l2006[[1]] - l2006[[2]]
 # oppure
 dvi2006 = l2006$defor2_.1 - l2006$defor2_.2
-dvi2006 # se clicco invio otterrò le informazioni riportate di seguito
+dvi2006 # cliccando invio si ottengo le informazioni riportate di seguito
 ## class      : RasterLayer 
 ## dimensions : 478, 717, 342726  (nrow, ncol, ncell)
 ## resolution : 1, 1  (x, y)
@@ -99,9 +106,9 @@ plot(dvi2006, col=cl)
 
 #DVI difference in time: differenza tra i valori DVI del 1992 rispetto a quello del 2006
 dvi_dif = dvi1992 - dvi2006
-cld <- colorRampPalette(c(“blue, “white”, “red”)) (100)
+cld <- colorRampPalette(c("blue", "white", "red")) (100)
 plot(dvi_dif, col=cld)
-# visualizzo una immagine in cui il rosso rappresenta uan differenza alta, altissima deforestazione
+# visualizzo una immagine in cui il rosso rappresenta una differenza alta, altissima deforestazione
 
 #############
 ## giorno 2
