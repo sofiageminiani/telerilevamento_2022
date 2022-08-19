7e6bf03ab933948b50623eb264ac9551dcef1834
sofiageminiani <100956833+sofiageminiani@users.noreply.github.com>
2022-08-19 14:15:14 +0200

    Create R_code_complete.r

---
+++ R_code_complete.r
@@ -0,0 +1,19 @@
+# R code complete Telerilevamento Geo_Ecologico
+
+#######################
+
+# Sommario
+
+# 1. Remote Sensing first code
+# 2. R code Time series
+# 3. R code Copernicus
+# 4. R code knitr
+# 5. R code Multivariate Analysis
+# 6. R code Classification
+# 7. R code ggplot2
+# 8. R code Vegetation Indices
+# 9. R code Land Cover
+# 10. R code Variability
+# 11. R code Spectral Signatures
+
+#######################
