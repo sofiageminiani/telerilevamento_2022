29d0b902af2c0d2465d85edf1eb0d514a457b777
sofiageminiani <100956833+sofiageminiani@users.noreply.github.com>
2022-08-19 14:13:07 +0200

    Create R_code_exam.r

---
+++ R_code_exam.r
@@ -0,0 +1,17 @@
+# esame di telerilevamento geo-ecologico 2022, Prof. Duccio Rocchini
+
+# area di studio:
+
+# settaggio della working directory
+
+
+# richiamo le librerie necessarie previa installazione dei relativi pacchetti
+
+# importazione delle immagini da analizzare
+
+#time series analysis 
+
+# calcolo DVI e NDVI per intervalli temporali
+
+# realizzazione di uma mappa di land cover
+# 
