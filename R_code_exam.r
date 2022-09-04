c94cd05bcf667a132832b75571ad4ee082a1a580
sofiageminiani <100956833+sofiageminiani@users.noreply.github.com>
2022-09-04 15:37:44 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -1,11 +1,19 @@
 # esame di telerilevamento geo-ecologico 2022, Prof. Duccio Rocchini
 
-# area di studio: Madagascar
-
-# settaggio della working directory
-
+# area di studio: Madagascar in particular Kirindy-Ambadira Forest Complex (KAFC), path and raw
 
 # richiamo le librerie necessarie previa installazione dei relativi pacchetti
+library(raster)
+library(RStoolbox)
+library(rgdal)
+library(rasterdiv)
+library(ggplot2)
+library(patchwork)
+
+# settaggio della working directory (Mac user)
+setwd("/Users/sofiageminiani/desktop/lab/Esame_telerilevamento") 
+
+
 
 # importazione delle immagini da analizzare
 
