42f23de45c236cd77f9d8f1f1dfd783c4f2181d3
sofiageminiani <100956833+sofiageminiani@users.noreply.github.com>
2022-09-07 23:58:51 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -36,7 +36,7 @@ mad2013 # cliccando invio ottengo le seguenti informazioni
 ## min values :                     0,                     0,                     0,                     0,                     0,                     0,                     0,                     0 
 ## max values :                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535 
 
-plot1<-ggRGB(mad2013, 4,3,2, stretch="lin")+labs(title="2013") #plot in RGB
+plot1<-ggRGB(mad2013, 4,3,2, stretch="hist")+labs(title="2013") #plot in RGB
 plot2<-ggRGB(mad2013, 5,4,3, stretch="hist")+labs(title="2013 IR") #falsi colori IR
 
 # 2016
@@ -53,7 +53,7 @@ mad2016 # cliccando invio ottengo le seguenti informazioni
 ## min values :                     0,                     0,                     0,                     0,                     0,                     0,                     0,                     0 
 ## max values :                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535 
 
-plot3<-ggRGB(mad2016, 4,3,2, stretch="lin")+labs(title="2016") # plot in RGB
+plot3<-ggRGB(mad2016, 4,3,2, stretch="hist")+labs(title="2016") # plot in RGB
 plot4<-ggRGB(mad2016, 5,4,3, stretch="hist")+labs(title="2016 IR") # falsi colori IR
 
 # 2019
@@ -70,13 +70,13 @@ mad2019 # cliccando invio ottengo le seguenti informazioni
 ## min values :                     0,                     0,                     0,                     0,                     0,                     0,                     0,                     0 
 ## max values :                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535 
 
-plot5<-ggRGB(mad2019, 4,3,2, stretch="lin")+labs(title="2019") # plot in RGB
+plot5<-ggRGB(mad2019, 4,3,2, stretch="hist")+labs(title="2019") # plot in RGB
 plot6<-ggRGB(mad2019, 5,4,3, stretch="hist")+labs(title="2019 IR") # falsi colori IR
 
 # 2021
 rlist<-list.files(pattern="LC08_L1TP_160074_20210521_20210529_02_T1_B") #creo una lista cercando elementi comuni
 import<-lapply(rlist,raster) #con lapply applico la funzione citata a tutta la lista
-mad2021<-stack(import) #unisce in un singolo file quelli che erano in lista
+mad2021<-stack(import) #unisce in un solo file le bande presenti nella lista
 mad2021 # cliccando invio ottengo le seguenti informazioni
 ## class      : RasterStack 
 ## dimensions : 7771, 7661, 59533631, 8  (nrow, ncol, ncell, nlayers)
@@ -87,7 +87,7 @@ mad2021 # cliccando invio ottengo le seguenti informazioni
 ## min values :                     0,                     0,                     0,                     0,                     0,                     0,                     0,                     0 
 ## max values :                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535 
 
-plot7<-ggRGB(mad2021, 4,3,2, stretch="lin")+labs(title="2021") # plot in RGB
+plot7<-ggRGB(mad2021, 4,3,2, stretch="hist")+labs(title="2021") # plot in RGB
 plot8<-ggRGB(mad2021, 5,4,3, stretch="hist")+labs(title="2021 IR") # falsi colori IR
 
 #plotto le 4 immagini per presentarle
@@ -96,19 +96,21 @@ plot1+plot3+plot5+plot7
 #plotto le 4 immagini per presentarle in IR
 plot2+plot4+plot6+plot8
 
-# calcolo DVI
+
+### SPECTRAL INDICES ###
+
+# calcolo indice spettrale DVI
 dvi2013 = mad2013[[5]]-mad2013[[4]]
 dvi2016 = mad2016[[5]]-mad2016[[4]]
 dvi2019 = mad2019[[5]]-mad2019[[4]]
 dvi2021 = mad2021[[5]]-mad2021[[4]]
 
-# calcolo NDVI
+# calcolo indice spettrale NDVI
 ndvi2013 = dvi2013 / (mad2013[[5]]+mad2013[[4]])
 ndvi2016 = dvi2016 / (mad2016[[5]]+mad2016[[4]])
 ndvi2019 = dvi2019 / (mad2019[[5]]+mad2019[[4]])
 ndvi2021 = dvi2021 / (mad2021[[5]]+mad2021[[4]])
-
-# differenza NDVI tra gli anni 2013 e 2021
+# differenza dell'indice spettrale NDVI tra gli anni 2013 e 2021
 cl <- colorRampPalette(c("blue", "white", "red")) (200)
 ndvi2013res<-resample(ndvi2013,ndvi2021,method="bilinear") # ricampionare per differente estensione dei due raster
 ndvi_diff <- ndvi2013res-ndvi2021
@@ -117,7 +119,25 @@ plot(ndvi2013, col=cl, main="NDVI 2013")
 plot(ndvi2021, col=cl, main="NDVI 2021")
 plot(ndvi_diff, col=cl, main="NDVI difference between 2013 and 2021")
 
+# calcolo indice spettrale GEMI
+# GEMI 2013
+n = (2 * ( mad2013[[5]] ^2 - mad2013[[4]] ^2) + 1.5 * mad2013[[5]] + 0.5 * mad2013[[4]] ) / ( mad2013[[5]] + mad2013[[4]] + 0.5 )
+gemi_2013c = (n*(1-0.25*n)-((mad2013[[4]]-0.125)/(1-mad2013[[4]])))
+# NIR = mad2013[[5]]
+# red = mad2013[[4]]
 
+# GEMI 2021
+n = ( 2 * ( mad2021[[5]] ^2 - mad2021[[4]] ^2) + 1.5 * mad2021[[5]] + 0.5 * mad2021[[4]] ) / ( mad2021[[5]] + mad2021[[4]] + 0.5 ))
+gemi_2021 = n*(1 - 0.25*n) - ((mad2021[[4]] - 0.125)/(1 - mad2021[[4]]))
+# NIR = mad2021[[5]]
+# red = mad2021[[4]]
+# GEMI difference between 2013 and 2021
+gemi2013res<-resample(gemi_2013,gemi_2021,method="bilinear") # ricampionare per differente estensione dei due raster
+gemi_diff <- gemi2013res-gemi_2021
+par(mfrow=c(3,1)) # realizzo un multiframe
+plot(gemi_2013, col=cl, main="GEMI 2013")
+plot(gemi_2021, col=cl, main="GEMI 2021")
+plot(gemi_diff, col=cl, main="GEMI difference between 2013 and 2021")
 
 # GVMI 2013 and 2021
 gvmi_2013 <- ((mad2013[[5]]+0.1)-(mad2013[[7]]+0.02))/((mad2013[[5]]+0.1)+(mad2013[[7]]+0.02))
@@ -125,23 +145,14 @@ gvmi_2021 <- ((mad2021[[5]]+0.1)-(mad2021[[7]]+0.02))/((mad2021[[5]]+0.1)+(mad20
 par(mfrow=c(2,1))
 plot(gvmi_2013)
 plot(gvmi_2021)
+# GVMI difference between 2013 and 2021
+gvmi2013res<-resample(gvmi_2013,gvmi_2021,method="bilinear") # ricampionare per differente estensione dei due raster
+gvmi_diff <- gvmi2013res-ndvi2021
+par(mfrow=c(3,1)) # realizzo un multiframe
+plot(gvmi_2013, col=cl, main="GVMI 2013")
+plot(gvmi_2021, col=cl, main="GVMI 2021")
+plot(gvmi_diff, col=cl, main="GVMI difference between 2013 and 2021")
 
-#GEMI 2013
-n = ( 2 * ( mad2013[[5]]^2 - mad2013[[4]]^2) + 1.5 * mad2013[[5]] + 0.5 * mad2013[[4]] ) / ( mad2013[[5]] + mad2013[[4]] + 0.5 )
-gemi_2013 = n*(1 - 0.25*n) - ((mad2013[[4]] - 0.125)/(1 - mad2013[[4]]))
-
-# NIR = mad2013[[5]]
-# red = mad2013[[4]]
-
-# GEMI 2021
-n = ( 2 * ( mad2021[[5]] ^2 - mad2021[[4]] ^2) + 1.5 * mad2021[[5]] + 0.5 * mad2021[[4]] ) / ( mad2021[[5]] + mad2021[[4]] + 0.5 )
-gemi_2021 = n*(1 - 0.25*n) - ((mad2021[[4]] - 0.125)/(1 - mad2021[[4]]))
-
-# NIR = mad2021[[5]]
-# red = mad2021[[4]]
-par(mfrow=c(2,1))
-plot(gemi_2013)
-plot(gemi_2021)
 
 # Realizzazione di una mappa di Land cover:
 # unsuperClass dell'immagine 2013
@@ -290,8 +301,6 @@ p9<-ggplot(multitemporal,aes(x=class,y=perc_2013, color=class))+
   geom_bar(stat="identity", color="black")+
   labs(x="Class",y="Percentage",title="Percentages for classification map of 2013")+
   theme(legend.position="bottom")
-
-
 
 
 
